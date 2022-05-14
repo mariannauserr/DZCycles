a3301fe0b1ed8510214fa69c71b6f1912ce831fa
Mariya Negretskaya <marian.93@mail.ru>
2022-05-14 11:11:15 +0300

    Testing data corrections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import ru.netology.sqr.SQRService;
 
 public class Main {
     public static void main(String[] args) {
-        SQRService service = new SQRService ();
+        SQRService service = new SQRService();
         int min = 200;
         int max = 300;
         int count = service.calculate(min, max);
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,4 +1,5 @@
 package ru.netology.sqr;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
